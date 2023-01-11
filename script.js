47a81fd9cb1653ffb63663793d5d48a1dca9bce5
Glitch (glitch-blank-website) <none>
2023-01-11 05:09:43 +0000

    🎻💁 Checkpoint ./style.css:4128163/451 ./index.html:4128163/538 ./script.js:4128163/262

---
+++ script.js
@@ -0,0 +1,7 @@
+let lastGeneratedTimestamp = -1;
+function generateEditorTimestamps(amount){
+  for(let i = 0; i < amount; i++){
+    let element = document.createElement("td");
+    element.innerHTML = lastGeneratedTimestamp 
+  }
+}
