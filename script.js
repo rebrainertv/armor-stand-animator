4001187aebca18236b9e7ad8df1e524b770c6c5f
Glitch (glitch-blank-website) <none>
2023-01-11 00:41:27 +0000

    🎤🐆 Checkpoint ./index.html:4128163/190 ./script.js:4128163/2609 ./style.css:4128163/56

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+let three = window.three || null;
+let previewContainer = document.getElementById("preview-container");
 
-console.log("Hello, world!");
+const scene = new three.Scene();
+const camera = new three.PerspectiveCamera( 75, previewContainer.clientWidth / previewContainer.clientrHeight, 0.1, 1000 );
+
+const renderer = new three.WebGLRenderer();
+renderer.setSize( previewContainer.clientWidth, previewContainer.clientrHeight );
+previewContainer.appendChild( renderer.domElement );
+
+const geometry = new three.BoxGeometry( 1, 1, 1 );
+const material = new three.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new three.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame( animate );
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+}
+animate();
