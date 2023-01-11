b7b3fe4e3808556d1edf3b692d0d66c593a1950c
Glitch (glitch-blank-website) <none>
2023-01-11 00:52:30 +0000

    😂📡 Checkpoint ./script.js:4128163/2

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let three = window.three || null;
 let previewContainer = document.getElementById("preview-container");
 
 const scene = new three.Scene();
-const camera = new three.PerspectiveCamera( 75, previewContainer.clientWidth / previewContainer.clientrHeight, 0.1, 1000 );
+const camera = new three.PerspectiveCamera( 75, previewContainer.clientWidth / previewContainer.clientHeight, 0.1, 1000 );
 
 const renderer = new three.WebGLRenderer();
-renderer.setSize( previewContainer.clientWidth, previewContainer.clientrHeight );
+renderer.setSize( previewContainer.clientWidth, previewContainer.clientHeight );
 previewContainer.appendChild( renderer.domElement );
 
 const geometry = new three.BoxGeometry( 1, 1, 1 );
