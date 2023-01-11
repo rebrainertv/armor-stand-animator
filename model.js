b175c6ca628b24334780efd545cbd02accf1ec33
Glitch (glitch-blank-website) <none>
2023-01-11 01:39:50 +0000

    👌😻 Checkpoint ./model.js:4128163/8118

--- model.js
+++ model.js
@@ -1,142 +1,140 @@
+import * as THREE from 'three';
 
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 
-			import * as THREE from 'three';
+let camera, scene, renderer;
+let gui;
 
-			import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
-			import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-			import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-			import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+const state = { variant: 'midnight' };
 
-			let camera, scene, renderer;
-			let gui;
+let previewContainer = document.getElementById("preview-container");
 
-			const state = { variant: 'midnight' };
+init();
+render();
 
-			init();
-			render();
+function init() {
 
-			function init() {
+  camera = new THREE.PerspectiveCamera( 45, previewContainer.clientWidth / previewContainer.clientHeight, 0.25, 20 );
+  camera.position.set( 0, 1, 3 );
+  
+  window.camera = camera;
+  window.render = render;
 
-				const container = document.createElement( 'div' );
-				document.body.appendChild( container );
+  scene = new THREE.Scene();
 
-				camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
-				camera.position.set( 2.5, 1.5, 3.0 );
+  new RGBELoader()
+    .setPath( 'https://cdn.glitch.global/4d97fd49-3058-498f-bd12-c7dacef119e3/' )
+    .load( 'quarry_01_1k.hdr', function ( texture ) {
 
-				scene = new THREE.Scene();
+      texture.mapping = THREE.EquirectangularReflectionMapping;
 
-				new RGBELoader()
-					.setPath( 'https://cdn.glitch.global/4d97fd49-3058-498f-bd12-c7dacef119e3/' )
-					.load( 'quarry_01_1k.hdr', function ( texture ) {
+      scene.background = texture;
+      scene.environment = texture;
 
-						texture.mapping = THREE.EquirectangularReflectionMapping;
+      render();
 
-						scene.background = texture;
-						scene.environment = texture;
+      // model
 
-						render();
+      const loader = new GLTFLoader().setPath( 'https://cdn.glitch.global/4d97fd49-3058-498f-bd12-c7dacef119e3/' );
+      loader.load( 'model.gltf', function ( gltf ) {
 
-						// model
+        gltf.scene.scale.set( 1.0, 1.0, 1.0 );
 
-						const loader = new GLTFLoader().setPath( 'https://cdn.glitch.global/4d97fd49-3058-498f-bd12-c7dacef119e3/' );
-						loader.load( 'model.gltf', function ( gltf ) {
+        scene.add( gltf.scene );
 
-							gltf.scene.scale.set( 10.0, 10.0, 10.0 );
+        /*// GUI
+        gui = new GUI();
 
-							scene.add( gltf.scene );
+        // Details of the KHR_materials_variants extension used here can be found below
+        // https://github.com/KhronosGroup/glTF/tree/master/extensions/2.0/Khronos/KHR_materials_variants
+        const parser = gltf.parser;
+        const variantsExtension = gltf.userData.gltfExtensions[ 'KHR_materials_variants' ];
+        const variants = variantsExtension.variants.map( ( variant ) => variant.name );
+        const variantsCtrl = gui.add( state, 'variant', variants ).name( 'Variant' );
 
-							// GUI
-							gui = new GUI();
+        selectVariant( scene, parser, variantsExtension, state.variant );
 
-							// Details of the KHR_materials_variants extension used here can be found below
-							// https://github.com/KhronosGroup/glTF/tree/master/extensions/2.0/Khronos/KHR_materials_variants
-							const parser = gltf.parser;
-							const variantsExtension = gltf.userData.gltfExtensions[ 'KHR_materials_variants' ];
-							const variants = variantsExtension.variants.map( ( variant ) => variant.name );
-							const variantsCtrl = gui.add( state, 'variant', variants ).name( 'Variant' );
+        variantsCtrl.onChange( ( value ) => selectVariant( scene, parser, variantsExtension, value ) );*/
 
-							selectVariant( scene, parser, variantsExtension, state.variant );
+        render();
 
-							variantsCtrl.onChange( ( value ) => selectVariant( scene, parser, variantsExtension, value ) );
+      } );
 
-							render();
+    } );
 
-						} );
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( previewContainer.clientWidth, previewContainer.clientHeight );
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 1;
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  previewContainer.appendChild( renderer.domElement );
 
-					} );
+  /*const controls = new OrbitControls( camera, renderer.domElement );
+  controls.addEventListener( 'change', render ); // use if there is no animation loop
+  controls.minDistance = 2;
+  controls.maxDistance = 50;
+  controls.target.set( 0, 10, - 0.2 );
+  controls.update();*/
 
-				renderer = new THREE.WebGLRenderer( { antialias: true } );
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				renderer.toneMapping = THREE.ACESFilmicToneMapping;
-				renderer.toneMappingExposure = 1;
-				renderer.outputEncoding = THREE.sRGBEncoding;
-				container.appendChild( renderer.domElement );
+  window.addEventListener( 'resize', onWindowResize );
 
-				const controls = new OrbitControls( camera, renderer.domElement );
-				controls.addEventListener( 'change', render ); // use if there is no animation loop
-				controls.minDistance = 2;
-				controls.maxDistance = 10;
-				controls.target.set( 0, 0.5, - 0.2 );
-				controls.update();
+}
 
-				window.addEventListener( 'resize', onWindowResize );
+function selectVariant( scene, parser, extension, variantName ) {
 
-			}
+  const variantIndex = extension.variants.findIndex( ( v ) => v.name.includes( variantName ) );
 
-			function selectVariant( scene, parser, extension, variantName ) {
+  scene.traverse( async ( object ) => {
 
-				const variantIndex = extension.variants.findIndex( ( v ) => v.name.includes( variantName ) );
+    if ( ! object.isMesh || ! object.userData.gltfExtensions ) return;
 
-				scene.traverse( async ( object ) => {
+    const meshVariantDef = object.userData.gltfExtensions[ 'KHR_materials_variants' ];
 
-					if ( ! object.isMesh || ! object.userData.gltfExtensions ) return;
+    if ( ! meshVariantDef ) return;
 
-					const meshVariantDef = object.userData.gltfExtensions[ 'KHR_materials_variants' ];
+    if ( ! object.userData.originalMaterial ) {
 
-					if ( ! meshVariantDef ) return;
+      object.userData.originalMaterial = object.material;
 
-					if ( ! object.userData.originalMaterial ) {
+    }
 
-						object.userData.originalMaterial = object.material;
+    const mapping = meshVariantDef.mappings
+      .find( ( mapping ) => mapping.variants.includes( variantIndex ) );
 
-					}
+    if ( mapping ) {
 
-					const mapping = meshVariantDef.mappings
-						.find( ( mapping ) => mapping.variants.includes( variantIndex ) );
+      object.material = await parser.getDependency( 'material', mapping.material );
+      parser.assignFinalMaterial( object );
 
-					if ( mapping ) {
+    } else {
 
-						object.material = await parser.getDependency( 'material', mapping.material );
-						parser.assignFinalMaterial( object );
+      object.material = object.userData.originalMaterial;
 
-					} else {
+    }
 
-						object.material = object.userData.originalMaterial;
+    render();
 
-					}
+  } );
 
-					render();
+}
 
-				} );
+function onWindowResize() {
 
-			}
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-			function onWindowResize() {
+  renderer.setSize( window.innerWidth, window.innerHeight );
 
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
+  render();
 
-				renderer.setSize( window.innerWidth, window.innerHeight );
+}
 
-				render();
+//
 
-			}
-
-			//
-
-			function render() {
-
-				renderer.render( scene, camera );
-
-			}
+function render() {
+  renderer.render( scene, camera );
+}
