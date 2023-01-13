8e00b5157e8eb61d0756f5b58f66a325f2b3b94a
Glitch (glitch-blank-website) <none>
2023-01-13 23:20:14 +0000

    👂🍩 Checkpoint ./index.html:4128163/360 ./style.css:4128163/138 ./script.js:4128163/1799

--- script.js
+++ script.js
@@ -63,3 +63,54 @@ function updateRotation(){
   
   render();
 }
+
+// Make the DIV element draggable:
+Array.from(document.querySelectorAll(".marker")).forEach((el) => {dragElement(el);})
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    //elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    
+    let leftValue = (elmnt.offsetLeft - pos1);
+    if(leftValue < 0){
+      pos1 = 0;
+      leftValue = 0;
+    } 
+    
+    elmnt.style.left = leftValue + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
